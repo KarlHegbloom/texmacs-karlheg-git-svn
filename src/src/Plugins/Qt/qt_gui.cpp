KarlHegbloom/texmacs-karlheg-git-svn

commit f55d0764562ff7e1b3c17266e1fc551b06a27651

Fix #38218 : Bug between copy/paste and auto bracket
git-svn-id: svn://svn.savannah.gnu.org/texmacs/trunk@6227 64cb5145-927a-446d-8aed-2fb7b4773692

src/src/Plugins/Qt/qt_gui.cpp
@@ -151,8 +151,10 @@ qt_gui_rep::get_selection (string key, tree& t, string& s, string format) {
 
   s= "";
   t= "none";
-  bool owns= false;
-  if (md->hasFormat ("application/x-texmacs-pid")) {
+  bool owns= !(format == "default" && key == "primary") &&
+             !(key == "mouse" && cb->supportsSelection ());
+
+  if (!owns && md->hasFormat ("application/x-texmacs-pid")) {
     buf= md->data ("application/x-texmacs-pid");
     if (!(buf.isEmpty())) {
       owns= string (buf.constData(), buf.size())

commit 02749ddbc2d2f2d6bd412c5a0bf00a1851a632de

Fix bug introduced in r6227
git-svn-id: svn+ssh://svn.sv.gnu.org/texmacs/trunk@6301 64cb5145-927a-446d-8aed-2fb7b4773692

src/src/Plugins/Qt/qt_gui.cpp
@@ -151,7 +151,8 @@ qt_gui_rep::get_selection (string key, tree& t, string& s, string format) {
 
   s= "";
   t= "none";
-  bool owns= !(format == "default" && key == "primary") &&
+  // Knowing when we owns (or not) the content is not clear
+  bool owns= (format != "temp" && format != "wrapbuf" && key != "primary") &&
              !(key == "mouse" && cb->supportsSelection ());
 
   if (!owns && md->hasFormat ("application/x-texmacs-pid")) {