KarlHegbloom/texmacs-karlheg-git-svn

commit 036015a9b2d344784411e61fbf4f9e171d247bd8

LaTeX export: indents some macros
git-svn-id: svn://svn.savannah.gnu.org/texmacs/trunk@6638 64cb5145-927a-446d-8aed-2fb7b4773692

src/TeXmacs/progs/convert/latex/tmtex-acm.scm
@@ -27,10 +27,11 @@
 (tm-define (tmtex-append-authors l)
   (:mode acm-style?)
   (if (null? l) l
-    (letrec ((sep (lambda (x)
-                    `(!concat (!linefeed) (alignauthor) (!linefeed) ,x))))
-      `((!document (numberofauthors ,(number->string (length l)))
-                   (author (!concat ,@(map sep (map cadr l)))))))))
+    (with n (number->string (length l))
+      (set! l (list-intersperse
+                (map cadr l) '(!concat (!linefeed) (alignauthor) (!linefeed))))
+      `((!document (numberofauthors ,n)
+                   (author (!indent (!concat (alignauthor) ,@l))))))))
 
 (tm-define (tmtex-make-author names affiliations emails urls miscs notes)
   (:mode acm-style?)

src/TeXmacs/progs/convert/latex/texout.scm
@@ -169,11 +169,23 @@
 			   (cons " " (cdr l))
 			   (cdr l))))))
 
+(define (texout-multiline? x)
+  (cond ((nlist? x) #f)
+        ((in? (car x) '(!begin !nextline !newline !linefeed !eqn !table)) #t)
+        ((and (in? (car x) '(!document !paragraph)) (> (length (cdr x)) 1)) #t)
+        ((npair? (cdr x)) #f)
+        (else (or (texout-multiline? (cadr x))
+                  (texout-multiline? `(!concat ,@(cddr x)))))))
+
 (define (texout-indent x)
-  (output-indent 2)
-  (texout x)
-  (output-indent -2)
-  (output-lf))
+  (if (texout-multiline? x)
+    (begin
+      (output-indent 2)
+      (output-lf)
+      (texout x)
+      (output-indent -2)
+      (output-lf))
+    (texout x)))
 
 (define (texout-linefeed)
   (output-lf))

src/TeXmacs/progs/convert/latex/tmtex.scm
@@ -1278,13 +1278,16 @@
       (tmtex-make-author names affiliations emails urls miscs notes))))
 
 (tm-define (tmtex-append-authors l)
-  (if (< (length l) 2) l
-    (with lf '(!concat (!linefeed) (and) (!linefeed))
-          `((author (!concat ,@(list-intersperse (map cadr l) lf)))))))
+  (cond ((null? l) '())
+        ((== (length l) 2) `(,(car l) (!indent (,(cdr l)))))
+        (else
+          (with lf '(!concat (!linefeed) (and) (!linefeed))
+            `((author
+                (!indent (!concat ,@(list-intersperse (map cadr l) lf)))))))))
 
 (define (tmtex-make-title titles subtitles notes miscs)
   (with titles (tmtex-concat-Sep (map cadr titles))
-        `(title (!paragraph ,titles ,@subtitles ,@notes ,@miscs))))
+        `(title (!indent (!paragraph ,titles ,@subtitles ,@notes ,@miscs)))))
 
 (tm-define (tmtex-make-doc-data titles subtitles authors dates miscs notes)
   `(!document

commit 095228890bd0bbf5f4256a2ab8e4a922aef8f49a

LaTeX export: ACM: Fix bug introduced in r6638
git-svn-id: svn://svn.savannah.gnu.org/texmacs/trunk@6640 64cb5145-927a-446d-8aed-2fb7b4773692

src/TeXmacs/progs/convert/latex/tmtex-acm.scm
@@ -31,7 +31,7 @@
       (set! l (list-intersperse
                 (map cadr l) '(!concat (!linefeed) (alignauthor) (!linefeed))))
       `((!document (numberofauthors ,n)
-                   (author (!indent (!concat (alignauthor) ,@l))))))))
+                   (author (!indent (!concat (alignauthor) " " ,@l))))))))
 
 (tm-define (tmtex-make-author names affiliations emails urls miscs notes)
   (:mode acm-style?)