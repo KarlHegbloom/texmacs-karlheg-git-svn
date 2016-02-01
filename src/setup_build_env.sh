a693429a4d5cd2cb5702c52adfcbc40b9fc419dd
Karl Hegbloom <karl.hegbloom@gmail.com>
2016-01-31 21:16:16 -0700

    Set up the build env for Guile and parallel building.

--- src/setup_build_env.sh
+++ src/setup_build_env.sh
@@ -13,6 +13,5 @@ export GUILE_DATA_PATH="`pkg-config --variable=datadir guile-1.8`"
 export GUILE_VERSION="`pkg-config --modversion guile-1.8`"
 
 #export CCACHE_PREFIX=/usr/bin/distcc
-#export DISTCC_HOSTS="localhost/8 192.168.1.64/4"
-#export DEB_BUILD_OPTIONS=parallel=12
-export DEB_BUILD_OPTIONS=parallel=8
+#export DISTCC_HOSTS="localhost/2 192.168.1.64/4"
+export DEB_BUILD_OPTIONS=parallel=4
