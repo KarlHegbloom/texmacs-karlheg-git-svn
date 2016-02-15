a90e25558f9b77840a0e1bfd81330ebf9d6fb69f
Karl Hegbloom <karl.hegbloom@gmail.com>
2016-02-15 09:26:17 -0700

    Updated

--- src/setup_build_env.sh
+++ src/setup_build_env.sh
@@ -3,6 +3,6 @@ export GUILE_LDFLAGS="`pkg-config --static --libs guile-1.8`"
 export GUILE_DATA_PATH="`pkg-config --variable=datadir guile-1.8`"
 export GUILE_VERSION="`pkg-config --modversion guile-1.8`"
 
-#export CCACHE_PREFIX=/usr/bin/distcc
-#export DISTCC_HOSTS="localhost/2 192.168.1.64/4"
-export DEB_BUILD_OPTIONS=parallel=4
+export CCACHE_PREFIX=/usr/bin/distcc
+export DISTCC_HOSTS="localhost/8 192.168.1.64/4"
+export DEB_BUILD_OPTIONS=parallel=12
