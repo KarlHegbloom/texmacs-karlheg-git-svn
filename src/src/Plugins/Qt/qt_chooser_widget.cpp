b687d449b1c82f1283be77c25ed7babf105fc847
Karl Hegbloom <karl.hegbloom@gmail.com>
2016-06-26 19:53:42 -0600

    Hack to make file dialog work again. Without this, it puts up a dialog, then an empty GUI box above it, but the dialog can't be activated. Closing the empty box closes the dialog.
    The printer dialog is segv right now also. This doesn't fix it.

--- src/src/Plugins/Qt/qt_chooser_widget.cpp
+++ src/src/Plugins/Qt/qt_chooser_widget.cpp
@@ -207,7 +207,7 @@ qt_chooser_widget_rep::perform_dialog () {
   c_string tmp (directory * "/" * file);
   QString path = QString::fromLocal8Bit (tmp);
   
-#if (defined(Q_WS_MAC) )// || defined(Q_WS_WIN)) //at least windows Xp and 7 lack image preview, switch to custom dialog
+#if 1 //#if (defined(Q_WS_MAC) )// || defined(Q_WS_WIN)) //at least windows Xp and 7 lack image preview, switch to custom dialog
   QFileDialog* dialog = new QFileDialog (NULL, caption, path);
 #else
   QTMFileDialog*  dialog;
@@ -265,7 +265,7 @@ qt_chooser_widget_rep::perform_dialog () {
       // here we need the filename encoded cork-universal because it is exposed in the document, in the <image> tag
       file = "(system->url " * scm_quote (localname) * ")";
       if (type == "image") {
-#if !defined(Q_WS_MAC) // && !defined(Q_WS_WIN)   //at least windows Xp and 7 lack image preview, switch to custom dialog
+#if 0 //#if !defined(Q_WS_MAC) // && !defined(Q_WS_WIN)   //at least windows Xp and 7 lack image preview, switch to custom dialog
         file = "(list " * file * imgdialog->getParamsAsString () * ")"; //set image size from preview
 #else //MacOs only now
         QPixmap pic (fileNames.first()); // Qt can't eps & pdf in windows.
