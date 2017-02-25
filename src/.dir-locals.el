41db6214d384fe3f308bc01f5a167f43a257a465
Karl Hegbloom <karl.hegbloom@gmail.com>
2017-02-24 19:52:01 -0700

    .dir-locals.el for Emacs

---
+++ src/.dir-locals.el
@@ -0,0 +1,166 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (cmake-ide-project-dir . "~/src/TeXmacs/texmacs-git-svn-guile-1.8/src")
+  (cmake-ide-build-dir . "~/src/TeXmacs/texmacs-git-svn-guile-1.8/src/obj-x86_64-linux-gnu")
+  (org-default-notes-file . "~/src/Juris-M/zotero-texmacs-integration/zotero-texmacs-integration_Notes.org")
+  (org-refile-targets
+   ("~/src/Juris-M/zotero-texmacs-integration/zotero-texmacs-integration_Notes.org" :maxlevel . 7)
+   (org-agenda-files :maxlevel . 7))
+  (eval . (setenv "GDB" "gdb -i=mi "))
+  (eval . (when (not (string-match "ccache" (getenv "PATH")))
+            (setenv "PATH" (string-join (list "/usr/lib/ccache:" (getenv "PATH"))))))
+  (eval . (setenv "DEB_BUILD_OPTIONS" (string-join (list "parallel=8 nostrip "
+                                                         (or (getenv "DEB_BUILD_OPTIONS") "")))))
+  (indent-tabs-mode . nil)
+  (fill-column . 132)
+  (org-startup-truncated . t)
+  (org-enforce-todo-dependencies . t)
+  (org-enforce-todo-checkbox-dependencies . t)
+  (org-closed-keep-when-no-todo . nil)
+  (org-treat-S-cursor-todo-selection-as-state-change . nil)
+  (org-refile-allow-creating-parent-nodes . confirm)
+  (org-id-link-to-org-use-id . create-if-interactive-and-no-custom-id)
+  (org-log-into-drawer . t)
+  (org-log-note-clock-out . t)
+  (org-log-redeadline . note)
+  (org-log-reschedule . note)
+  (org-lowest-priority . 68)
+  (org-priority-faces (65 . "red"))
+  (org-use-sub-superscripts . {})
+  (org-todo-keyword-faces
+   ("FIXME" . "Magenta")
+   ("NEXT" . "HotPink")
+   ("WIP" . "HotPink")
+   ("WAITING" . "DarkOrange")
+   ("TESTING" . "OrangeRed")
+   ("TESTING" . "gold")
+   ("VERIFY" . "red")
+   ("CANCELED" . "OliveDrab")
+   ("CANCELLED" . "OliveDrab")
+   ("S.E.P." . "DarkOrange"))
+  (org-todo-keywords
+   (sequence "TODO(t)" "WIP(w!)" "WAITING(W@/!)" "TESTING(T!)" "VERIFY(v@/!)" "FIXME(f@/!)" "|" "DONE(d!/!)" "CANCELED(c@/!)" "S.E.P.(s@/!)")
+   (sequence "To Do: " "Done : "))
+  (org-export-allow-bind-keywords . t)
+  (org-export-with-email . t)
+  (org-export-with-smart-quotes . t)
+  (org-export-with-toc . nil)
+  (org-export-with-tags . nil)
+  (org-export-with-todo-keywords . nil)
+  (org-export-select-tags "export")    ;; also #+SELECT_TAGS: keyword
+  (org-export-exclude-tags "noexport") ;; also #+EXCLUDE_TAGS: keyword
+  (org-export-snippet-translation-alist
+   ("h" . "html") ("l" . "latex") ("m" . "markdown"))
+  (org-fontify-done-headline . t)
+  (org-fontify-whole-heading-line . t)
+  (org-hidden-keywords author date email title)
+  (org-image-actual-width 960)
+  (org-html-allow-name-attribute-in-anchors . t)
+  (org-html-checkbox-type . unicode)
+  (org-html-doctype . "html5")
+  (org-html-head .
+"<style type=\"text/css\">
+/*--><![CDATA[/*><!--*/
+body {
+  font-size: medium;
+  line-height: 150%;
+  text-align: justify;
+  margin-top: 2%;
+  margin-left: 2%;
+  margin-right: 2%;
+  margin-bottom: 2%;
+}
+.sc {
+  font-variant: small-caps;
+}
+h1.title {
+  font-size: x-large !important;
+}
+.subtitle {
+  font-size: large !important;
+  padding-top: 0.5in;
+}
+.casetable {
+  margin-bottom: 0.5in;
+  margin-left: 5%;
+  font-size: large;
+  line-height: normal;
+}
+.outline-2 {
+  margin-left: auto;
+  margin-right: auto;
+}
+.outline-2 h2 {
+  font-size: medium;
+  margin-bottom: 0;
+}
+.outline-text-2 {
+  margin-left: 2%;
+  margin-right: 2%;
+}
+.outline-text-2 p {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+.outline-text-2 p a {
+  margin-left: -1%;
+  font-weight: bold;
+  font-style: italic;
+}
+.outline-3 {
+  margin-left: 2%;
+  margin-right: 2%;
+}
+.outline-3 h3 {
+  margin-bottom: 0;
+}
+.outline-text-3 {
+  margin-left: 2%;
+  margin-right: 2%;
+}
+.outline-text-3 p {
+  margin-top: 0;
+  /* margin-bottom: 0; */
+}
+.outline-4 {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+.outline-4 h4 {
+  margin-left: 2%;
+  margin-right: 2%;
+  margin-top: 0;
+  margin-bottom: 0;
+}
+.outline-text-4 {
+  line-height: normal;
+  margin-left: 3%;
+  margin-right: 3%;
+  font-size: 90%
+}
+.outline-text-4 p {
+  margin-top: 0;
+  /* margin-bottom: 0; */
+}
+/*]]>*/-->\n</style>")
+  (org-html-head-extra .
+"<!--[if lt IE 9]>
+  <script src=\"javascripts/html5shiv-printshiv.min.js\"></script>
+<![endif]-->")
+  (org-html-html5-fancy . t)
+  (org-html-text-markup-alist
+   (bold . "<b>%s</b>")
+   (code . "<span class=\"sc\">%s</span>")
+   (italic . "<i>%s</i>")
+   (strike-through . "<del>%s</del>")
+   (underline . "<span class=\"underline\">%s</span>")
+   (verbatim . "<code>%s</code>"))
+  (org-latex-text-markup-alist
+   (bold . "\\textbf{%s}")
+   (code . "\\textsc{%s}")
+   (italic . "\\emph{%s}")
+   (strike-through . "\\sout{%s}")
+   (underline . "\\uline{%s}")
+   (verbatim . protectedtexttt))))
