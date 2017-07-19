7ff0f320169f23271394163330af63890db64a40
vdhoeven <vdhoeven@64cb5145-927a-446d-8aed-2fb7b4773692>
2017-07-19 10:59:03 +0000

    Better cleaning
    git-svn-id: svn://svn.savannah.gnu.org/texmacs/trunk@10775 64cb5145-927a-446d-8aed-2fb7b4773692

--- src/plugins/maxima/progs/maxima-menus.scm
+++ src/plugins/maxima/progs/maxima-menus.scm
@@ -24,6 +24,9 @@
 ;; Several subroutines for the evaluation of Maxima expressions
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(define (maxima-spaces? t)
+  (and (string? t) (in? t (list "" " " "  " "   " "    "))))
+
 (define (maxima-prompt? t)
   (or (match? t '(text (with "font-family" "tt" "color" "red" :*)))
       (match? t '(with "font-family" "tt" "color" "red" :*))
@@ -45,6 +48,8 @@
 	 (maxima-output-simplify (cAr t)))
 	((func? t 'with)
 	 (rcons (cDr t) (maxima-output-simplify (cAr t))))
+        ((and (func? t 'concat) (pair? (cdr t)) (maxima-spaces? (cadr t)))
+         (maxima-output-simplify (cons (car t) (cddr t))))
 	((func? t 'concat)
 	 (apply tmconcat (map maxima-output-simplify (cdr t))))
 	(else (plugin-output-std-simplify "maxima" t))))
